0cedfc95955a938362c0fe7180512f8c27432d47
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-06-20 11:17:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ function UpdateClock() {
   var hh = date.getHours();
   var mm = date.getMinutes();
   var ss = date.getSeconds();
-  let dnum=date.getDate();
-    var yy = date.getFullYear();
+  let dnum = date.getDate();
+  var yy = date.getFullYear();
   var mon = date.getMonth();
   var dd = date.getDay();
-  am_pm = "PM";
+  am_pm = "AM";
   if (hh > 12) {
     hh -= 12;
-    am_pm = "AM";
+    am_pm = "PM";
   }
   if (hh == 0) {
     hr = 12;
     am_pm = "AM";
   }
 
-    hh = hh < 10 ? "0" + hh : hh;
-    mm = mm < 10 ? "0" + mm : mm;
-    ss = ss < 10 ? "0" + ss : ss;
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
 
   var months = [
     "January",
@@ -44,8 +44,8 @@ function UpdateClock() {
     "Friday",
     "Saturday",
   ];
-  var ids = ["dd", "mon", "dnum","yy", "hh", "mm", "ss", "AP"];
-  var values = [weeks[dd], months[mon],dnum, yy, hh, mm, ss, am_pm];
+  var ids = ["dd", "mon", "dnum", "yy", "hh", "mm", "ss", "AP"];
+  var values = [weeks[dd], months[mon], dnum, yy, hh, mm, ss, am_pm];
   for (var i = 0; i < ids.length; i++)
     document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
